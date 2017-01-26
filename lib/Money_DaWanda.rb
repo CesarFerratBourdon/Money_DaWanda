c86e28c4e67e50c62fd1148fdc97043534fd2cfc
Cesar Ferrat <cesar.ferrat1@gmail.com>
2017-01-26 18:49:26 +0100

    added .convert_to method

--- lib/Money_DaWanda.rb
+++ lib/Money_DaWanda.rb
@@ -25,5 +25,20 @@ class Money
     end
   end
 
+  def convert_to(new_currency)
+    # check if conversion rate exists inside @@conversion_rate table
+    if @@conversion_rates.has_key?[@currency] && @@conversion_rate[@currency].has_key?[new_currency]
+      rate = @@conversion_rates[@currency][new_currency]
+    #if not, but reversed rate exists, use it
+    elsif @@conversion_rates.has_key?[new_currency] && @@conversion_rate[new_currency].has_key?[@currency]
+      rate = 1 / @@conversion_rates[new_currency][@currency]
+    else
+      return "This currency exchange is unfortunately not available"
+    end
+
+    new_amount = @amount * rate
+    return Money.new(new_amount,new_currency)
+  end
+
 
 end
