7a6c95e978addc633a66ca7e6ca0ac3340f565b6
Cesar Ferrat <cesar.ferrat1@gmail.com>
2017-01-26 23:59:45 +0100

    added 4 Arithmetics methods

--- lib/Money_DaWanda.rb
+++ lib/Money_DaWanda.rb
@@ -5,6 +5,9 @@ class Money
   @@conversion_rates = {}
 
   def initialize(amount, currency)
+    raise ArgumentError, 'Amount is not numeric' unless amount.is_a? Numeric
+    raise ArgumentError, 'Currency should be a string' unless currency.is_a? String
+
     @amount = amount
     @currency = currency
   end
@@ -14,6 +17,9 @@ class Money
   end
 
   def self.add_conversion_rates(currency_base, rates)
+    raise ArgumentError, 'Currency should be a string' unless currency_base.is_a? String
+    raise ArgumentError, 'Rates should be a hash' unless rates.is_a? Hash
+
     rates.each do |currency,value|
       @@conversion_rates[currency_base][currency] = value
     end
@@ -26,6 +32,7 @@ class Money
   end
 
   def convert_to(new_currency)
+    raise ArgumentError, 'Currency should be a string' unless new_currency.is_a? String
     # check if conversion rate exists inside @@conversion_rate table
     if @@conversion_rates.has_key?[@currency] && @@conversion_rate[@currency].has_key?[new_currency]
       rate = @@conversion_rates[@currency][new_currency]
@@ -36,9 +43,38 @@ class Money
       return "This currency exchange is unfortunately not available"
     end
 
-    new_amount = @amount * rate
+    new_amount = (@amount * rate).round(2)
     return Money.new(new_amount,new_currency)
   end
+
+  def +(money)
+    if money.currency != @currency && money.amount != 0
+      return Money.new(@amount + (money.convert_to(@currency)).amount, @currency)
+    elsif money.amount != 0  #if money.amount == 0 we don't even bother creating a new Money instance
+      return Money.new(@amount + money.amount, @currency)
+    end
+  end
+
+  def -(money)
+    if money.currency != @currency && money.amount != 0
+      return Money.new(@amount - (money.convert_to(@currency)).amount, @currency)
+    elsif money.amount != 0  #if money.amount == 0 we don't even bother creating a new Money instance
+      return Money.new(@amount - money.amount, @currency)
+    end
+  end
+
+  def *(number)
+    raise ArgumentError, 'Number should be a non nil numeric'  unless number.is_a?(Numeric) and number != 0
+      return Money.new((@amount * number).round(2), @currency)
+  end
+
+  def /(number)
+    raise ArgumentError, 'Number should be a non nil numeric'  unless number.is_a?(Numeric) and number != 0
+      return Money.new((@amount / number).round(2), @currency)
+  end
+
+
+
 
 
 end
